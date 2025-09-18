ef760d16e7e40fe81adeb50252801a19234cfcbf
Andew Negrut <andrewnegrut@gmail.com>
2025-09-18 10:53:21 -0500

    makefile done

---
+++ client.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, I'm the client.\n");
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, I'm the server.\n");
+    return 0;
+}
